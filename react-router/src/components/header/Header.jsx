b9486108e6cbf084c68be2e52db45e9e1133b1a8
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-19 21:45:55 +0600

    learned NavLink, useNavigation & useLocation

--- react-router/src/components/header/Header.jsx
+++ react-router/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
@@ -7,8 +7,8 @@ const Header = () => {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact Us</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
       </nav>
     </div>
   );
--- react-router/src/components/home/Home.jsx
+++ react-router/src/components/home/Home.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 
 const Home = () => {
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <Header />
-      <Outlet />
+      {navigation.state === "loading" ? <p>Loading...</p> : <Outlet />}
       <Footer />
     </div>
   );
