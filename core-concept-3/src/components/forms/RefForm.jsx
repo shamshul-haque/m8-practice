416fe7d4fb635518551fab7d76c17791f8cebf89
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-22 11:51:49 +0600

    learned to handle controlled form

--- core-concept-3/src/components/forms/RefForm.jsx
+++ core-concept-3/src/components/forms/RefForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+/* import { useEffect, useRef } from "react";
 
 const RefForm = () => {
   const handleSubmit = (e) => {
@@ -31,3 +31,4 @@ const RefForm = () => {
 };
 
 export default RefForm;
+ */
